3aa099c314789dc6941903d8af545459620ec01e
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-29 21:59:01 +0200

    <button onClick={() => setTheme('light')}>Light</button>

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,12 +2,15 @@ import { useContext } from "react";
 import { MainContext } from "../mainContext";
 
 const Settings = () => {
-  const { theme } = useContext(MainContext);
+  const { theme, setTheme } = useContext(MainContext);
   return (
-
     <div className="page pageConfig">
-      Theme: {theme}
+      <div className="row">
+        Theme: ({theme})
+        <button onClick={() => setTheme('light')}>Light</button>
+        <button onClick={() => setTheme('dark')}>Dark</button>
       </div>
-  ) 
+    </div>
+  );
 };
 export default Settings;
