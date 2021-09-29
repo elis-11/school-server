5fce92491b418b627870fbf51487dcbd12576091
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-29 20:42:55 +0200

    npm i sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+import "./App.scss";
+import Config from "./pages/Config";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      jjjj
+      <Router>
+        <nav>
+          <ul>
+            <li><NavLink className="selected" to="/" exact={true}>Home</NavLink></li>
+            <li><NavLink className="selected" to="/Config">Config</NavLink> </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/config" >
+            <Config />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
