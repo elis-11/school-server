087c34948b166c6fe29f18314947608a333cf08e
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-29 21:39:19 +0200

    const [theme, setTheme] = useState('light')

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
+import {useState} from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink
+  NavLink,
 } from "react-router-dom";
 import "./App.scss";
+import { MainContext } from './mainContext';
 import Config from "./pages/Config";
 import Home from "./pages/Home";
 
 function App() {
+  const [theme, setTheme] = useState('light')
   return (
     <div className="App">
+      <MainContext.Provider value={{theme, setTheme}} >
       <Router>
         <nav>
           <ul>
-            <li><NavLink activeClassName="selected" to="/" exact={true}>Home</NavLink></li>
-            <li><NavLink activeClassName="selected" to="/Config">Config</NavLink> </li>
+            <li>
+              <NavLink activeClassName="selected" to="/" exact={true}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="selected" to="/Config">
+                Config
+              </NavLink>{" "}
+            </li>
           </ul>
         </nav>
         <Switch>
           <Route path="/" exact>
             <Home />
           </Route>
-          <Route path="/config" >
+          <Route path="/config">
             <Config />
           </Route>
         </Switch>
       </Router>
+        </MainContext.Provider>
     </div>
   );
 }
