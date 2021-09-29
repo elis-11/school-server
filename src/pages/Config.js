2e353146d0697c7da265ab7cda2e9e59b216d9e8
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-29 21:34:54 +0200

    const { theme } = useContext(MainContext);

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -1,6 +1,13 @@
-const Config =()=>{
-  return(
-      <div className='page pageConfig'>Config...</div>
-  )
-}
-export default Config
+import { useContext } from "react";
+import { MainContext } from "../mainContext";
+
+const Config = () => {
+  const { theme } = useContext(MainContext);
+  return (
+
+    <div className="page pageConfig">
+      Theme: {theme}
+      </div>
+  ) 
+};
+export default Config;
