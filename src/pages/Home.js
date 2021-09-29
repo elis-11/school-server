92222eab728d1d797b556ccfb730ba4fd2eb0dfa
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-29 21:49:22 +0200

    Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,15 @@
+import { useContext } from "react";
+import { MainContext } from "../mainContext";
+
+
 const Home =()=>{
+  const { theme } = useContext(MainContext);
   return(
-      <div className='page pageHome'>Home...</div>
+      <div className='page pageHome'>
+        <p>Welcome to this site.</p>
+        <hr />
+        <p>The theme is currently: {theme}</p>
+      </div>
   )
 }
 export default Home
