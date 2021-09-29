409cdc7273df6c8bba955859b9278cf7305ba4f6
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-29 22:12:05 +0200

    Language: ({language})

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,13 +2,18 @@ import { useContext } from "react";
 import { MainContext } from "../mainContext";
 
 const Settings = () => {
-  const { theme, setTheme } = useContext(MainContext);
+  const { theme, setTheme, language, setLanguage } = useContext(MainContext);
   return (
     <div className="page pageConfig">
       <div className="row">
         Theme: ({theme})
         <button onClick={() => setTheme('light')}>Light</button>
         <button onClick={() => setTheme('dark')}>Dark</button>
+      </div>
+      <div className="row">
+        Language: ({language})
+        <button onClick={() => setLanguage('english')}>Emglish</button>
+        <button onClick={() => setLanguage('german')}>German</button>
       </div>
     </div>
   );
