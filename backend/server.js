b76077c6a8851e25d9280e0996cf22a3c7e7c6ca
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-26 11:29:32 +0200

    route.rest

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors'
+import fs from 'fs';
 
 const app = express()
 const port = 3023
@@ -9,4 +10,25 @@ app.use(express.json())
 
 app.listen(port, ()=> {
     console.log(`Listening on port ${port}`);
+
+    app.get('/getpin', (req, res) => {
+        fs.readFile('./backend/data/pin.txt', 'utf-8', (err, data) => {
+            res.status(200).send({
+                pin: data
+            });
+        });
+    });
+     
+    app.post('/savepin', (req, res) => {
+        const newpin = req.body.pin;
+        fs.writeFile('./backend/data/pin.txt', newpin, (err) => {
+            res.status(200).json({
+                success: true
+            });
+        });
+    });
+     
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}.`);
+    });
 })
