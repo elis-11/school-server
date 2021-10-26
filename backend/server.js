afbe6dedf7736814241fab58820c2c4c9ffff485
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-26 12:30:18 +0200

    done second part

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,30 @@
-import express from 'express';
-import cors from 'cors'
-import fs from 'fs';
+import express from "express";
+import cors from "cors";
+import fs from "fs";
 
-const app = express()
-const port = 3023
+const app = express();
+const port = 3023;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, ()=> {
-    console.log(`Listening on port ${port}`);
-
-    app.get('/getpin', (req, res) => {
-        fs.readFile('./backend/data/pin.txt', 'utf-8', (err, data) => {
-            res.status(200).send({
-                pin: data
-            });
-        });
+app.get("/getpin", (req, res) => {
+  fs.readFile("./backend/data/pin.txt", "utf-8", (err, data) => {
+    res.status(200).send({
+      pin: data,
     });
-     
-    app.post('/savepin', (req, res) => {
-        const newpin = req.body.pin;
-        fs.writeFile('./backend/data/pin.txt', newpin, (err) => {
-            res.status(200).json({
-                success: true
-            });
-        });
+  });
+});
+
+app.post("/savepin", (req, res) => {
+  const newpin = req.body.pin;
+  fs.writeFile("./backend/data/pin.txt", newpin, (err) => {
+    res.status(200).json({
+      success: true,
     });
-     
-    app.listen(port, () => {
-        console.log(`Listening on port ${port}.`);
-    });
-})
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [pin, setPin] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:3023/getpin");
+      const data = await response.json();
+      setPin(data.pin);
+    })();
+  }, []);
+  const handlePinChange = (e) => {
+    setPin(e.target.value);
+  };
+
+  const handleSave = (e) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ pin }),
+    };
+    (async () => {
+      await fetch(`http://localhost:3023/savepin`, requestOptions);
+    })();
+  };
+
   return (
     <div className="App">
-tralala
+      <input type="text" value={pin} onChange={handlePinChange} />{" "}
+      <button onClick={handleSave}>Save</button>
     </div>
   );
 }
