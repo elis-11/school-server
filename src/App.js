1fa37681c7054997e99c83ea48ca972a068afaa7
elis-11 <eliza.arzanukaeva@gmail.com>
2021-12-12 17:40:32 +0100

    first step in to form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+const Step1 = () => <>Step1</>;
+const Step2 = () => <>Step2</>;
+const Step3 = () => <>Step3</>;
+const Result = () => <>Result</>;
+
+const Layout = () => <>Layout</>;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-liss
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Step1 />} />
+          <Route path="step2" element={<Step2 />} />
+          <Route path="step3" element={<Step3 />} />
+          <Route path="result" element={<Result />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
