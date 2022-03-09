f12ab916d4bf28b82b140639860f878094700290
elis-11 <eliza.arzanukaeva@gmail.com>
2022-03-09 09:38:40 +0100

    new changes

--- WebDev/wd1.js
+++ WebDev/wd1.js
@@ -1,3 +1,13 @@
 let isSet=[1,2,3,1,3,5,'Hi',5,3,7,9,11]
 let itsSet=new Set(isSet)
 console.log(itsSet);
+
+// Most important array methods
+// students.find
+// students.filter
+// students.forEach
+// students.map
+// students.push
+// students.pop
+// students.splice
+
