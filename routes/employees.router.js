77071be1729d9ce63d0532ecd925df19581f3f67
“elis-11” <eliza.arzanukaeva@gmail.com>
2022-06-28 00:17:46 +0200

    Added functional to Emloyee

--- routes/employees.router.js
+++ routes/employees.router.js
@@ -8,3 +8,11 @@ employeesRouter.get("/", async (req, res) => {
   const employeeAll = await Employee.find();
   res.json(employeeAll);
 });
+
+//Get single employee
+employeesRouter.get("/:id", async (req, res) =>{
+const employeeId=req.params.id;
+console.log({employeeId});
+const employee=await Employee.findById(employeeId);
+res.json(employee);
+})
--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import { studentsRouter } from "./routes/students.router.js";
 import { teachersRouter } from "./routes/teachers.router.js";
 import { employeesRouter } from "./routes/employees.router.js";
+import session from "express-session";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
 
+app.use(cors({ origin: process.env.FRONTEND_ORIGIN, credentials: true }));
 app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    proxy: true,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.send(`
 <h2>Welcome!</h2>
 <div>Our routes:</div>
 <div>Home: <a href="/">/</a></div>
-<div>Teachers: <a href="/teachers">/teachers</a></div>
 <div>Employees: <a href="/employees">/employees</a></div>
+<div>Teachers: <a href="/teachers">/teachers</a></div>
 <div>Students: <a href="/students">/students</a></div>
     `);
 });
@@ -31,7 +48,7 @@ app.use((req, res, next) => {
   res.status(404).json({ error: `This route does not exist` });
 });
 
-const PORT = 5555 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
   console.log(`App listenin at http://localhost:` + PORT);
 });
